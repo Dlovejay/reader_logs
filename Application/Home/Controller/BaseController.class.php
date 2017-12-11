1586b619bb5719b5470f86c4ebb8add9f1e02815
丁鹏飞 <dingpengfei@dingpengfeideMacBook-Pro.local>
2017-12-11 14:46:22 +0800

    basecontroller需要实例化一下父类的构造函数

--- Application/Home/Controller/BaseController.class.php
+++ Application/Home/Controller/BaseController.class.php
@@ -6,6 +6,7 @@ class BaseController extends Controller
 {
     public function __construct()
     {
+        parent::__construct();
         if(empty($_SESSION['user_info'])){
             $this->redirect('index/index', [], 2, '请先登录！');
         }
