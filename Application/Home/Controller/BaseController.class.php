4fa9f96a109fa2b72fd84892788edbd040a468fe
丁鹏飞 <dingpengfei@dingpengfeideMacBook-Pro.local>
2017-12-11 14:45:16 +0800

    数据库配置

--- Application/Home/Controller/BaseController.class.php
+++ Application/Home/Controller/BaseController.class.php
@@ -6,7 +6,6 @@ class BaseController extends Controller
 {
     public function __construct()
     {
-        dump($_SESSION['user_info']);
         if(empty($_SESSION['user_info'])){
             $this->redirect('index/index', [], 2, '请先登录！');
         }
